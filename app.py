2bcebaa07b90f35a54f42250bc68176834c6deb0
tugceakin <tugce.eylul.akin@hotmail.com>
2016-04-19 01:27:25 -0700

    /rateRestaurant route is added

--- app.py
+++ app.py
@@ -9,12 +9,30 @@ from bson import BSON
 from bson import json_util
 
 connection = MongoClient("mongodb://localhost:27017")
-
 db = connection.recommender 
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+
+@app.route('/rateRestaurant', methods=['POST'])
+def rate_restaurant():
+    reviews = db.reviews
+    business_ratings = db.business_ratings
+    user_ratings = db.user_ratings
+    rating_data = json.loads(request.data)
+    rating = rating_data["rating"]
+    user_id = rating_data["user_id"]
+    business_id = rating_data["business_id"]
+
+    reviews.insert( { "user_id": user_id, "business_id": business_id, "stars": rating})
+    user_business = user_id + "." + business_id
+    user_ratings.update( {"_id": user_id}, { "$set": { user_business: rating}})
+    business_user = business_id + "." + user_id
+    business_ratings.update( {"_id": business_id}, { "$set": { business_user: rating}})
+
+    resp = make_response("", 201)
+    return resp
 
 @app.route('/searchRestaurants', methods=['GET', 'POST'])
 def get_restaurants_by_name():
