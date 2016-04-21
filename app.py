b54f1becb70ec28a75644ce1d1edc1298c901288
tugceakin <tugce.eylul.akin@hotmail.com>
2016-04-21 12:25:28 -0700

    Recommendation system is implemented

--- app.py
+++ app.py
@@ -8,25 +8,41 @@ import datetime
 from pymongo import MongoClient
 from bson import BSON
 from bson import json_util
+from recommendations import getRecommendations
 
 connection = MongoClient("mongodb://localhost:27017")
 db = connection.recommender 
+reviews = db.reviews
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+@app.route('/getRecommendations', methods=['POST'])
+def get_recommendations():
+    user_data = json.loads(request.data)
+    items = getRecommendations(user_data["user_id"])
+    resp = make_response(json.dumps(list(items), sort_keys=True, indent=4, default=json_util.default))
+    return resp
+
+@app.route('/getRatedRestaurants', methods=['POST'])
+def get_rated_restaurants():
+    user_data = json.loads(request.data)
+    user_id = user_data["user_id"]
+    restaurants = reviews.find({"user_id": user_id})#.sort([("date", -1)])
+    resp = make_response(json.dumps(list(restaurants), sort_keys=True, indent=4, default=json_util.default))
+    return resp
 
 @app.route('/rateRestaurant', methods=['POST'])
 def rate_restaurant():
-    reviews = db.reviews
     business_ratings = db.business_ratings
     user_ratings = db.user_ratings
     rating_data = json.loads(request.data)
-    rating = rating_data["rating"]
+    rating_data["date"] =  datetime.datetime.now()
+    rating = rating_data["stars"]
     user_id = rating_data["user_id"]
     business_id = rating_data["business_id"]
-
-    reviews.insert( { "user_id": user_id, "business_id": business_id, "stars": rating, "date": datetime.datetime.now()})
+    
+    reviews.insert(rating_data)
     user_business = user_id + "." + business_id
     user_ratings.update( {"_id": user_id}, { "$set": { user_business: rating}})
     business_user = business_id + "." + user_id
